11bb019d4c93353aa20a3158fc57ed41f0479629
Karl2301 <79416282+Karl2301@users.noreply.github.com>
2025-05-13 13:16:45 +0000

    Add force validation endpoint to handle user status updates

---
+++ app/routes/forceValidation.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, session, make_response, jsonify, abort
+from ext_config import *
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlmodel import Session, select
+import json
+import uuid
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from ds import send_discord_message
+from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
+
+
+def force_validation():
+    """
+    data = request.get_json()  # Récupérer le contenu du POST en JSON
+    identifiant = data.get('identifiant')
+    """
+    data = request.get_json()
+    force = data.get('force')
+    session_cookie = request.cookies.get('session_cookie')
+    classe = data.get('classe')
+
+    with Session(engine) as sessionuser:
+        user = get_user_by_cookie(sessionuser, session_cookie)
+        
+        if not user:
+            return jsonify({"error": "Utilisateur non trouvé."}), 404
+        
+        # Vérifier si l'utilisateur est déjà connecté
+        if user.deja_connecte == True:
+            return jsonify({"error": "L'utilisateur est déjà connecté."}), 400
+        
+        if user.professeur == False:
+            return jsonify({"error": "Accès refusé. Seuls les professeurs peuvent forcer la validation."}), 403
+        
+        # Mettre à jour le statut de l'utilisateur
+        with Session(engine) as sessionuser:
+            users = sessionuser.exec(select(Users).where(Users.classe == classe)).all()
+            if not user:
+                return jsonify({"error": "Utilisateur non trouvé."}), 404
+            
+            for user in users:
+                app.logger.info(f"Utilisateur trouvé : {user.identifiant_unique}")
+
+        sessionuser.add(user)
+        sessionuser.commit()
+        
+        return jsonify({"message": "Statut de l'utilisateur mis à jour avec succès."}), 200
--- app/static/js/classe_prof/main.js
+++ app/static/js/classe_prof/main.js
@@ -285,7 +285,7 @@ document.addEventListener("DOMContentLoaded", function () {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ "force": true })
+            body: JSON.stringify({ "force": true , "classe": window.location.pathname.split('/')[2]})
             });
 
             if (response.ok) {
